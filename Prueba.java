251a57ff9dadb3641f5b171f31fe5dccd89e2ea5
Angel <iangeleb19@gmail.com>
2022-10-10 09:14:21 +0200

    Muestra mi comida favorita

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,7 @@ public class Prueba {
 	public static void main(String args[]) {
 		System.out.println("Erazo Brito");
 		System.out.println("19 años del 2003");
+		System.out.println("Comida Favotira, los espaguetis");
 
 	}
 
